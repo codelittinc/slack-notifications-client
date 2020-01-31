7076e99d06d730b2d02c66b09909b01569e4f863
kaiomagalhaes <me@kaiomagalhaes.com>
2020-01-31 14:59:28 -0300

    create findChannelByName method

--- src/services/Slack.js
+++ src/services/Slack.js
@@ -3,16 +3,10 @@ import SlackApi from '@slack/web-api';
 class Slack {
   static async sendChannelMessage({ message, channelName, ts }) {
     const token = process.env.SLACK_API_KEY;
-    console.log(message, channelName, token)
 
     const slackClient = new SlackApi.WebClient(token);
 
-    const response = await slackClient.channels.list({
-      limit: 500
-    });
-
-    let channels = response.channels.sort((a, b) => a.created < b.created)
-    const channel = channels.find(c => c.name === channelName)
+    const channel = await this.findChannelByName(slackClient, channelName);
 
     const res = await slackClient.chat.postMessage({
       channel: channel.id,
@@ -26,6 +20,16 @@ class Slack {
 
     return res;
   };
+
+  static async findChannelByName(slackClient, channelName) {
+    const response = await slackClient.channels.list({
+      limit: 1000
+    });
+
+    const { channels } = response;
+
+    return channels.find(c => c.name === channelName)
+  }
 };
 
 export default Slack;
